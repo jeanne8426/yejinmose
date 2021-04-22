3853aef9a178683bd06afad36bcd1fa3c391344f
yejin <jeanne8426@naver.com>
2021-04-23 05:22:08 +0900

    first

---
+++ script/script.js
@@ -0,0 +1,46 @@
+/*1*/
+$(".list1").click(function(){
+   $(".layer1").css("display","block");
+});
+
+$(".layer1 > .close1").click(function(){
+   $(".layer1").css("display","none");
+});
+
+/*2*/
+$(".list2").click(function(){
+   $(".layer2").css("display","block");
+});
+
+$(".layer2 > .close2").click(function(){
+   $(".layer2").css("display","none");
+});
+
+/*3*/
+$(".list3").click(function(){
+   $(".layer3").css("display","block");
+});
+
+$(".layer3 > .close3").click(function(){
+   $(".layer3").css("display","none");
+});
+
+
+/*4*/
+$(".list4").click(function(){
+   $(".layer4").css("display","block");
+});
+
+$(".layer4 > .close4").click(function(){
+   $(".layer4").css("display","none");
+});
+
+
+/*5*/
+$(".list5").click(function(){
+   $(".layer5").css("display","block");
+});
+
+$(".layer5 > .close5").click(function(){
+   $(".layer5").css("display","none");
+});
